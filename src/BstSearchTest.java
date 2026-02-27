8127158475aa07794612e482e96f0689642edccd
Darien Arthur-Gocken <233949874+DarienArthur-Gocken@users.noreply.github.com>
2026-02-27 10:01:32 -0800

    value found / not found test

--- src/BstSearchTest.java
+++ src/BstSearchTest.java
@@ -15,4 +15,27 @@ public class BstSearchTest {
 	void testEmptyTree() {
 		assertFalse(BstSearch.contains(null, 5));
 	}
+
+	@Test
+	void testValueFound() {
+		BinaryTreeNode<Integer> root = new BinaryTreeNode<>(10);
+		root.left = new BinaryTreeNode<>(5);
+		root.right = new BinaryTreeNode<>(15);
+
+		assertTrue(BstSearch.contains(root, 5));
+		assertTrue(BstSearch.contains(root, 10));
+		assertTrue(BstSearch.contains(root, 15));
+	}
+
+	@Test
+    void testValueNotFound() {
+        BinaryTreeNode<Integer> root = new BinaryTreeNode<>(10);
+        root.left = new BinaryTreeNode<>(5);
+        root.right = new BinaryTreeNode<>(15);
+
+        assertFalse(BstSearch.contains(root, 3));
+        assertFalse(BstSearch.contains(root, 12));
+        assertFalse(BstSearch.contains(root, 20));
+    }
+
 }
