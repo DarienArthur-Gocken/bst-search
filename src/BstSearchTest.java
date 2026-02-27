8708e00db17faea5a22fac94ec16d5b68baa1702
Darien Arthur-Gocken <233949874+DarienArthur-Gocken@users.noreply.github.com>
2026-02-27 09:59:36 -0800

    no target test

--- src/BstSearchTest.java
+++ src/BstSearchTest.java
@@ -2,5 +2,12 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class BstSearchTest {
+	@Test
+	void testNoTarget() {
+	BinaryTreeNode<Integer> testNode = new BinaryTreeNode<>(1);
 
+	assertThrows(NullPointerException.class, () -> {
+		BstSearch.contains(testNode, null);
+	});
+	}
 }
